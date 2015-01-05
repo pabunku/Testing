ab0b5dc102eb10ee42ff2b408227c003525de41e
pabunku <pabunku@gmail.com>
2015-01-04 22:47:22 -0500

    This is a test

---
+++ test.py
@@ -0,0 +1,22 @@
+
+# x = '{0:.2%}'.format(.15)
+# print x
+one = [54, 32, 37]
+two = [4,5,6]
+three = [7,8,9]
+four = [10,11,12]
+counts = []
+groups = [one, two, three, four]
+for x in groups:
+	counts.append(x)
+	
+print len(counts)
+
+# for x in counts:
+	# for y in range(len(counts[x])):
+		# print y
+	
+thing = [[],[],[],[]]
+print thing
+thing[0].insert(0, 7)
+print thing
